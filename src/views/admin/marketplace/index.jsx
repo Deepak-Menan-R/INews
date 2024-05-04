62f99f45abe5f3ab0eece451c3398049d3eb0a5a
Deepak-Menan-R <deepakmenan2012@gmail.com>
2024-05-04 13:16:25 +0530

    Conditional rendring of categories

--- src/views/admin/marketplace/index.jsx
+++ src/views/admin/marketplace/index.jsx
@@ -6,6 +6,9 @@ import TopCreatorTable from "./components/TableTopCreators";
 
 const Marketplace = () => {
   const [news, setNews] = useState([]);
+  const [cateNews, setcateNews] = useState([]);
+  const [showCategory, setShowCategory] = useState(false);
+  const [cate, setcate] = useState('');
 
   useEffect(() => {
     fetchDataFromBackend();
@@ -54,36 +57,96 @@ const Marketplace = () => {
     return acc;
   }, {});
 
+  const toggleShowCategory = async (category) => {
+    setShowCategory(!showCategory);
+    setcate(category.toLowerCase())
+    if (category != null) {
+      try {
+        const user_id = localStorage.getItem('user_id');
+        const response = await fetch('http://localhost:5000/cate_post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ "user_id": user_id, "category": category.toLowerCase() })
+        });
+        if (response.ok) {
+          const jsonData = await response.json();
+          setcateNews(jsonData.categry_articles);
+          console.log('Data fetched:', jsonData.categry_articles);
+          // Handle the fetched data here
+        } else {
+          console.error('Error fetching data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+  };
+  
+
   return (
     <div className="mt-3 grid h-full grid-cols-1 gap-5 xl:grid-cols-2 2xl:grid-cols-3">
       <div className="col-span-1 h-fit w-full xl:col-span-1 2xl:col-span-2">
-        {/* Categories with respective articles */}
-        {Object.entries(groupedNews).map(([category, articles]) => (
-          <div key={category}>
-            <div className="mb-4 mt-5 flex flex-col justify-between px-4 md:flex-row md:items-center">
-              <h4 className="ml-1 text-2xl font-bold text-navy-700 dark:text-white">
-                {category} {/* Display category name */}
-              </h4>
-              <hr className=" border-b border-navy-700 dark:border-white" /> {/* Full-width underline */}
-              <button className="text-brand-500 font-medium hover:underline focus:outline-none">
-                Read More
-              </button>
-            </div>
-            {/* NFTs trending card for each category */}
-            <div className="z-20 grid grid-cols-1 gap-5 md:grid-cols-3">
-              {articles.map((article, index) => (
-                <NftCard
-                  key={index}
-                  title={article.title}
-                  author={article.source.name}
-                  image={article.urlToImage}
-                  description={article.description}
-                  url={article.url}
-                />
-              ))}
-            </div>
-          </div>
-        ))}
+        
+
+<div>
+  {showCategory ? (    
+    <>
+    <div className="mb-4 mt-5 flex flex-col justify-between px-4 md:flex-row md:items-center">
+          <h4 className="ml-1 text-2xl font-bold text-navy-700 dark:text-white">
+            {cate} {/* Display category name */}
+          </h4>
+          <hr className=" border-b border-navy-700 dark:border-white" /> {/* Full-width underline */}
+          <button onClick={() => toggleShowCategory(null)} className="text-brand-500 font-medium hover:underline focus:outline-none">
+            Back
+          </button>
+        </div>
+    <div className="z-20 grid grid-cols-1 gap-5 md:grid-cols-3">
+          {cateNews.map((article, index) => (
+            <NftCard
+              key={index}
+              title={article.title}
+              author={article.source.name}
+              image={article.urlToImage}
+              description={article.description}
+              url={article.url}
+            />
+          ))}
+        </div>
+      
+    </>
+  ) : (
+    Object.entries(groupedNews).map(([category, articles]) => (
+      <div key={category}>
+        <div className="mb-4 mt-5 flex flex-col justify-between px-4 md:flex-row md:items-center">
+          <h4 className="ml-1 text-2xl font-bold text-navy-700 dark:text-white">
+            {category} {/* Display category name */}
+          </h4>
+          <hr className=" border-b border-navy-700 dark:border-white" /> {/* Full-width underline */}
+          <button onClick={() => toggleShowCategory(category)} className="text-brand-500 font-medium hover:underline focus:outline-none">
+            Read More
+          </button>
+        </div>
+        {/* NFTs trending card for each category */}
+        <div className="z-20 grid grid-cols-1 gap-5 md:grid-cols-3">
+          {articles.map((article, index) => (
+            <NftCard
+              key={index}
+              title={article.title}
+              author={article.source.name}
+              image={article.urlToImage}
+              description={article.description}
+              url={article.url}
+            />
+          ))}
+        </div>
+      </div>
+    ))
+  )}
+</div>
+
+
       </div>
 
       {/* right side section */}
